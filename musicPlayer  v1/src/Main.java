66a53b32411e18a8106bbf3a9f753b808d41ed3e
Evoque <99081397+MaqC254@users.noreply.github.com>
2023-01-13 23:13:06 +0300

    Add files via upload

---
+++ musicPlayer  v1/src/Main.java
@@ -0,0 +1,50 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        File file1 = new File("Free Me - NEFFEX.wav");
+        try {
+            AudioInputStream audioInput = AudioSystem.getAudioInputStream(file1);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInput);
+            String choice = "";
+            while (!choice.equals("Q")) {
+                System.out.println("Enter your choice\n P = Play , K = Pause , R = Restart clip , Q = Quit");
+                choice = scanner.next();
+                choice = choice.toUpperCase();
+                long songMinute = 0;
+                switch (choice) {
+                    case "P":
+                        clip.start();
+                        break;
+                    case "K":
+                        clip.stop();
+                        break;
+                    case "R":
+                        clip.setMicrosecondPosition(0);
+                        break;
+                    case "Q":
+                        System.out.println("Goodbye");
+                        clip.stop();
+                        clip.close();
+                        break;
+                    default:
+                        System.out.println("Enter a valid option");
+                }
+            }
+            System.out.println("See you soon");
+
+
+        } catch (UnsupportedAudioFileException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (LineUnavailableException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
